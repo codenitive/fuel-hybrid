e6af57d1b6d23e6bfc05b69a364e93cdca68eacc
crynobone <crynobone@gmail.com>
2011-05-01 21:41:50 +0800

    Merge code changes from fuel/core

--- classes/pagination.php
+++ classes/pagination.php
@@ -1,7 +1,6 @@
 <?php
+
 /**
- * Fuel
- *
  * Fuel is a fast, lightweight, community driven PHP5 framework.
  *
  * @package		Fuel
@@ -12,8 +11,6 @@
  */
 
 namespace Hybrid;
-
-
 
 class Pagination {
 
@@ -112,7 +109,7 @@ class Pagination {
 
 		static::$total_pages = ceil(static::$total_items / static::$per_page) ?: 1;
 
-		static::$current_page = (int) \URI::segment(static::$uri_segment);
+		is_null(static::$current_page) and static::$current_page = (int) \URI::segment(static::$uri_segment);
 
 		if (static::$current_page > static::$total_pages)
 		{
@@ -143,14 +140,35 @@ class Pagination {
 		}
 
 		$pagination = '';
+		$pagination .= '&nbsp;'.static::prev_link('&laquo; Previous').'&nbsp;&nbsp;';
+		$pagination .= static::page_links();
+		$pagination .= '&nbsp;'.static::next_link('Next &raquo;');
 
+		return $pagination;
+	}
+	
+	// --------------------------------------------------------------------
+
+	/**
+	 * Pagination Page Number links
+	 *
+	 * @access public
+	 * @return mixed    Markup for page number links
+	 */
+	public static function page_links()
+	{
+		if (static::$total_pages == 1)
+		{
+			return '';
+		}
+		
+		$pagination = '';
+		
 		// Let's get the starting page number, this is determined using num_links
 		$start = ((static::$current_page - static::$num_links) > 0) ? static::$current_page - (static::$num_links - 1) : 1;
 
 		// Let's get the ending page number
 		$end   = ((static::$current_page + static::$num_links) < static::$total_pages) ? static::$current_page + static::$num_links : static::$total_pages;
-
-		$pagination .= '&nbsp;'.static::prev_link('&laquo Previous').'&nbsp;&nbsp;';
 
 		for($i = $start; $i <= $end; $i++)
 		{
@@ -164,8 +182,6 @@ class Pagination {
 				$pagination .= \Html::anchor(rtrim(static::$pagination_url, '/') . $url . '/' . static::$suffix_url, $i);
 			}
 		}
-
-		$pagination .= '&nbsp;'.static::next_link('Next &raquo;');
 
 		return $pagination;
 	}
@@ -233,10 +249,12 @@ class Pagination {
 		{
 			$data = \Hybrid\Input::get($value);
 			
-			if (empty($data)) {
+			if (empty($data))
+			{
 				return false;
 			}
-			else {
+			else 
+			{
 				array_push($dataset, sprintf('%s=%s', $value, $data));
 				return;
 			}
