4d446fb1cd1526aa231b6a15da011e7dd9266577
crynobone <crynobone@gmail.com>
2011-08-15 18:55:58 +0800

    Partial #18: progress 40%

--- classes/cart.php
+++ classes/cart.php
@@ -23,7 +23,6 @@ namespace Hybrid;
  * @package     Fuel
  * @subpackage  Hybrid
  * @category    Cart
- * @author      Mior Muhammad Zaki <crynobone@gmail.com>
  */
 
  class Cart {
@@ -39,44 +38,152 @@ namespace Hybrid;
     public static function factory($name = null)
     {
         // set instance name to default if null given
-        if (is_null($name) or !is_string($name) or empty($name))
+        if (is_null($name) or !\is_string($name) or empty($name))
         {
             $name = \Config::get('app.identity', 'fuelapp');
         }
 
-        if (is_null(static::$instances[$name]))
+        if (\is_null(static::$instances[$name]))
         {
-            static::$instances[$name] = new static();
+            static::$instances[$name] = new static($name);
         }
 
         return static::$instances[$name];
     }
 
-    protected $cart_contents = array();
+    protected $name     = null;
 
-    protected function __construct($config = null)
+    protected $contents = array(
+        'data'      => array(),
+        'total'     => 0.00,
+        'quantity'  => 0,
+    );
+
+    protected function __construct($name)
     {
-        $initconfig = \Config::load('app.cart', array());
-        $config     = array_merge($config, $initconfig);
-        
+        $this->name     = $name;
 
         // get available cart content (if available)
-        $this->cart_content = \Session::get('_cart_content', array());
+        $contents = \Session::get('_cart_content_' . $name, null);
+
+        if (\is_null($contents))
+        {
+            $contents['data']       = array();
+            $contents['total']      = 0.00;
+            $contents['quantity']   = 0;
+        }
+
+        $this->contents = $contents;
     }
 
+    /**
+     * Insert items into the cart and save it to the session
+     *
+     * @access  public
+     * @param   array   $items
+     */
     public function insert($items = array())
     {
-        
+        if (empty($items) or !\is_array($items))
+        {
+            throw new \Fuel_Exception("Item doesn't contain proper format, please provide an array");
+            return false;
+        }
+
+        $save_cart  = false;
+
+        if (isset($items['id']))
+        {
+            if (true === $this->_insert($items))
+            {
+                $save_cart = true;
+            }
+        }
+        else 
+        {
+            foreach ($items as $item)
+            {
+                if (\is_array($item) and isset($item['id']))
+                {
+                    if (true === $this->_insert($item))
+                    {
+                        $save_cart = true;
+                    }
+                }
+            }
+        }
+
+        if (true === $save_cart)
+        {
+            return $this->save();
+        }
+
+        return false;
     }
 
-    public function update()
+    /**
+     * Insert to self::contents
+     *
+     * @access  protected
+     * @param   array   $items
+     * @return  bool
+     * @throws  \Fuel_Exception
+     */
+    protected function _insert($items = array())
+    {
+        if (empty($items) or !\is_array($items))
+        {
+            throw new \Fuel_Exception("Item doesn't contain proper format, please provide an array");
+            return false;
+        }
+
+
+    }
+
+    public function update($items = array())
     {
         
     }
 
-    public function checkout()
+    protected function _update($item)
     {
         
+    }
+
+    /**
+     * Save all cart
+     *
+     * @access  public
+     * @return  bool
+     */
+    public function save()
+    {
+        
+        \Session::set('_cart_content_' . $this->name, $this->contents);
+
+        return true;
+    }
+
+    /**
+     * Get cart content based on type
+     *
+     * @access  public
+     * @param   string  $type   should be either "data", "quantity" or "total"
+     * @return  array
+     * @throws  \Fuel_Exception
+     */
+    public function get($type = null)
+    {
+        $type = trim(strtolower(strval($type)));
+
+        if (\array_key_exists($type, $this->contents))
+        {
+            return $this->contents[$type];
+        }
+        else
+        {
+            throw new \Fuel_Exception("Trying to access undefined `{$type}`");
+        }
     }
 
     /**
@@ -87,8 +194,9 @@ namespace Hybrid;
      */
     public function destroy()
     {
-        $this->cart_contents = array();
-        \Session::set('_cart_content', array());
+        // clear all value and database
+        $this->contents = array();
+        \Session::set('_cart_content_' . $this->name, array());
 
         return true;
     }
