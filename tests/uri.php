eeaf7a0eee299e3d5599b127a399ebf39cfdd68c
crynobone <crynobone@gmail.com>
2011-10-03 13:31:37 +0800

    Add Uri testcase

--- tests/uri.php
+++ tests/uri.php
@@ -26,11 +26,32 @@ class Test_Uri extends \Fuel\Core\TestCase {
     public function setup() 
     {
         \Fuel::add_package('hybrid');
+
+        $_GET = array(
+            'hello' => 'world',
+            'foo'   => 'bar',
+            'fuel'  => 'php',
+        );
     }
 
-    public function test_foo() 
+    public function test_build_get_query() 
     {
-        
+        $output = \Hybrid\Uri::build_get_query(array('foo', 'hello'));
+        $expected = '?foo=bar&hello=world';
+
+        $this->assertEquals($expected, $output);
+
+        $output = \Hybrid\Uri::build_get_query(array('foo', 'unavailable'));
+        $expected = '?foo=bar';
+
+        $this->assertEquals($expected, $output);
+    }
+    public function test_build_get_query_given_empty() 
+    {
+        $output = \Hybrid\Uri::build_get_query(array('unavailable'));
+        $expected = '?';
+
+        $this->assertEquals($expected, $output);
     }
 
 }
