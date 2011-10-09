e79f0ed2769b278899003559c62c0084e3ae3907
Kavinsky <nmunozfernandez@gmail.com>
2011-10-09 22:12:18 +0200

    added Currency class it convert using google calculator api, it will need a rework for the request when the curl class will be completed.

---
+++ tests/currency.php
@@ -0,0 +1,45 @@
+<?php
+
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.0
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2011 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Hybrid;
+
+/**
+ * Hybrid 
+ * 
+ * A set of class that extends the functionality of FuelPHP without 
+ * affecting the standard workflow when the application doesn't actually 
+ * utilize Hybrid feature.
+ *
+ * @package     Fuel
+ * @subpackage  Hybrid
+ * @category    Factory
+ * @category    Test
+ * @author      Mior Muhammad Zaki <crynobone@gmail.com>
+ */
+
+class Test_Factory extends \Fuel\Core\TestCase 
+{
+    /**
+     * Setup the test
+     */
+    public function setup()
+    {
+        \Package::load('hybrid');
+    }
+	
+	public function test_forge()
+	{
+		//@todo make the test when the curl class is released
+	}
+}
+	
