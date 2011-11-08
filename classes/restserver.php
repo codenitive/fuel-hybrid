d8218cff417a703418bc9edc02815f3f8b8fc281
crynobone <crynobone@gmail.com>
2011-11-08 23:57:15 +0800

    Update code to reflect changes in fuel/core

--- classes/restserver.php
+++ classes/restserver.php
@@ -410,7 +410,7 @@ class Restserver
         // Otherwise, check the HTTP_ACCEPT (if it exists and we are allowed)
         $http_accept = Input::server('HTTP_ACCEPT');
 
-        if (\Config::get('rest.ignore_http_accept') !== false and $http_accept)
+        if (\Config::get('rest.ignore_http_accept') === false and $http_accept)
         {
 
             // Check all formats against the HTTP_ACCEPT header
