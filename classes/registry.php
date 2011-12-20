4da612a4384f842c44a9ff1993c93308d4e41e62
crynobone <crynobone@gmail.com>
2011-12-20 23:43:47 +0800

    Initial commit for Hybrid\Registry
    Signed-off-by: crynobone <crynobone@gmail.com>

---
+++ classes/registry.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Fuel is a fast, lightweight, community driven PHP5 framework.
+ *
+ * @package    Fuel
+ * @version    1.1
+ * @author     Fuel Development Team
+ * @license    MIT License
+ * @copyright  2010 - 2011 Fuel Development Team
+ * @link       http://fuelphp.com
+ */
+
+namespace Hybrid;
+
+/**
+ * Hybrid 
+ * 
+ * A set of class that extends the functionality of FuelPHP without 
+ * affecting the standard workflow when the application doesn't actually 
+ * utilize Hybrid feature.
+ * 
+ * Registry Class
+ * 
+ * @package     Fuel
+ * @subpackage  Hybrid
+ * @category    Registry
+ * @author      Mior Muhammad Zaki <crynobone@gmail.com>
+ */
+
+class Registry 
+{
+
+}
