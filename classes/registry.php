b115231ac172aee8d78b6b4944b12edd6a2feba3
crynobone <crynobone@gmail.com>
2011-12-26 15:22:38 +0800

    Add Hybrid\Registry classs
    Signed-off-by: crynobone <crynobone@gmail.com>

--- classes/registry.php
+++ classes/registry.php
@@ -30,39 +30,115 @@ namespace Hybrid;
 
 class Registry 
 {
-	protected $instances = array();
+	/**
+	 * Cache registry instance so we can reuse it
+	 * 
+	 * @static
+	 * @access  protected
+	 * @var     array
+	 */
+	protected static $instances = array();
 
+	protected static function _init()
+	{
+		\Config::load('hybrid', 'hybrid');
+	}
+
+	/**
+	 * Shortcode to self::make().
+	 * 
+	 * @static
+	 * @access  public
+	 * @param   string  $name       instance name
+	 * @return  self::make()
+	 */
 	public static function forge($name = null)
 	{
 		return static::make($name);	
 	}
 
+	/**
+	 * Initiate a new Registry instance
+	 * 
+	 * @static
+	 * @access  public
+	 * @param   string  $name       instance name
+	 * @return  object
+	 */
 	public static function make($name = null)
 	{
-		
+		if (null === $name)
+		{
+			$name = 'default';
+		}
+
+		if ( ! isset(static::$instances[$name]))
+		{
+			static::$instances[$name] = new static();
+		}
+
+		return static::$instances[$name];
 	}
 
+	/**
+	 * @access  protected
+	 * @var     array   collection of key-value pair of either configuration or data
+	 */
 	protected $data = array();
 
-	protected function __construct()
+	/**
+	 * @access  protected
+	 * @var     string  storage configuration, currently only support runtime.
+	 */
+	protected $storage = 'runtime';
+
+	/**
+	 * Construct an instance.
+	 *
+	 * @access  protected
+	 * @param   string  $storage    set storage configuration (default to 'runtime').
+	 */
+	protected function __construct($storage = 'runtime') 
 	{
-		
+		$this->storage = $storage;
 	}
 
-	public function get($key = null)
+	/**
+	 * Get value of a key
+	 *
+	 * @access  public
+	 * @param   string  $key        A string of key to search.
+	 * @param   mixed   $default    Default value if key doesn't exist.
+	 * @return  mixed
+	 */
+	public function get($key, $default = null)
 	{
-		
+		return \Arr::get($this->data, $key, $default);
 	}
 
-	public function __get($key)
+	/**
+	 * Set a value from a key
+	 *
+	 * @access  public
+	 * @param   string  $key        A string of key to add the value.
+	 * @param   mixed   $value      The value.
+	 * @return  void
+	 */
+	public function set($key, $value = '')
 	{
-		
+		\Arr::set($this->data, $key, $value);
 	}
 
-	public function __set($key, $value)
+	/**
+	 * Delete value of a key
+	 *
+	 * @access  public
+	 * @param   string  $key        A string of key to delete.
+	 * @return  bool
+	 */
+	public function delete($key)
 	{
-		
+		return \Arr::delete($this->data, $key);
 	}
-
 
 }
